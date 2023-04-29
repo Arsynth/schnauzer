6cbed4ac19c4733239461b3e1a5f867539212088
Artem Sechko <arsynthdev@gmail.com>
2023-04-30 02:14:56 +0700

    Content filling for sections part 4

--- src/types/load_command.rs
+++ src/types/load_command.rs
@@ -1,10 +1,67 @@
 use super::constants::*;
 use super::RcReader;
 use super::Result;
+use crate::result::Error;
 use scroll::IOread;
 
 use std::fmt::Debug;
 use std::io::{Seek, SeekFrom};
+
+pub const LC_REQ_DYLD: u32 = 0x80000000;
+
+pub const LC_SEGMENT: u32 = 0x1;
+pub const LC_SYMTAB: u32 = 0x2;
+pub const LC_SYMSEG: u32 = 0x3;
+pub const LC_THREAD: u32 = 0x4;
+pub const LC_UNIXTHREAD: u32 = 0x5;
+pub const LC_LOADFVMLIB: u32 = 0x6;
+pub const LC_IDFVMLIB: u32 = 0x7;
+pub const LC_IDENT: u32 = 0x8;
+pub const LC_FVMFILE: u32 = 0x9;
+pub const LC_PREPAGE: u32 = 0xa;
+pub const LC_DYSYMTAB: u32 = 0xb;
+pub const LC_LOAD_DYLIB: u32 = 0xc;
+pub const LC_ID_DYLIB: u32 = 0xd;
+pub const LC_LOAD_DYLINKER: u32 = 0xe;
+pub const LC_ID_DYLINKER: u32 = 0xf;
+pub const LC_PREBOUND_DYLIB: u32 = 0x10;
+pub const LC_ROUTINES: u32 = 0x11;
+pub const LC_SUB_FRAMEWORK: u32 = 0x12;
+pub const LC_SUB_UMBRELLA: u32 = 0x13;
+pub const LC_SUB_CLIENT: u32 = 0x14;
+pub const LC_SUB_LIBRARY: u32 = 0x15;
+pub const LC_TWOLEVEL_HINTS: u32 = 0x16;
+pub const LC_PREBIND_CKSUM: u32 = 0x17;
+
+pub const LC_LOAD_WEAK_DYLIB: u32 = 0x18 | LC_REQ_DYLD;
+
+pub const LC_SEGMENT_64: u32 = 0x19;
+pub const LC_ROUTINES_64: u32 = 0x1a;
+pub const LC_UUID: u32 = 0x1b;
+pub const LC_RPATH: u32 = 0x1c | LC_REQ_DYLD;
+pub const LC_CODE_SIGNATURE: u32 = 0x1d;
+pub const LC_SEGMENT_SPLIT_INFO: u32 = 0x1e;
+pub const LC_REEXPORT_DYLIB: u32 = 0x1f | LC_REQ_DYLD;
+pub const LC_LAZY_LOAD_DYLIB: u32 = 0x20;
+pub const LC_ENCRYPTION_INFO: u32 = 0x21;
+pub const LC_DYLD_INFO: u32 = 0x22;
+pub const LC_DYLD_INFO_ONLY: u32 = 0x22 | LC_REQ_DYLD;
+pub const LC_LOAD_UPWARD_DYLIB: u32 = 0x23 | LC_REQ_DYLD;
+pub const LC_VERSION_MIN_MACOSX: u32 = 0x24;
+pub const LC_VERSION_MIN_IPHONEOS: u32 = 0x25;
+pub const LC_FUNCTION_STARTS: u32 = 0x26;
+pub const LC_DYLD_ENVIRONMENT: u32 = 0x27;
+pub const LC_MAIN: u32 = 0x28 | LC_REQ_DYLD;
+pub const LC_DATA_IN_CODE: u32 = 0x29;
+pub const LC_SOURCE_VERSION: u32 = 0x2A;
+pub const LC_DYLIB_CODE_SIGN_DRS: u32 = 0x2B;
+pub const LC_ENCRYPTION_INFO_64: u32 = 0x2C;
+pub const LC_LINKER_OPTION: u32 = 0x2D;
+pub const LC_LINKER_OPTIMIZATION_HINT: u32 = 0x2E;
+pub const LC_VERSION_MIN_TVOS: u32 = 0x2F;
+pub const LC_VERSION_MIN_WATCHOS: u32 = 0x30;
+pub const LC_NOTE: u32 = 0x31;
+pub const LC_BUILD_VERSION: u32 = 0x32;
 
 /// Represents general load command struct - `load_command`
 #[derive(Debug)]
@@ -42,6 +99,34 @@ impl LoadCommand {
 /// Load command has variable set of fields dependent to `cmd` field
 /// List of load commands here - <https://opensource.apple.com/source/xnu/xnu-4570.71.2/EXTERNAL_HEADERS/mach-o/loader.h.auto.html>
 pub enum LcVariant {
+    /// LC_SEGMENT
+    Segment(LcSegment),
+    /// LC_SEGMENT_64
+    Segment64(LcSegment64),
+    /// LC_ID_DYLIB, LC_LOAD_{,WEAK_}DYLIB, LC_REEXPORT_DYLIB
+    Dylib(LcDylib),
+    /// LC_SUB_FRAMEWORK
+    Subframework(LcSubframework),
+    /// LC_SUB_CLIENT
+    Subclient(LcSubclient),
+    /// LC_SUB_UMBRELLA
+    Subumbrella(LcSubumbrella),
+    /// LC_SUB_LIBRARY
+    Sublibrary(LcSublibrary),
+    /// LC_PREBOUND_DYLIB
+    PreboundDylib(LcPreboundDylib),
+    /// LC_ID_DYLINKER, LC_LOAD_DYLINKER, LC_DYLD_ENVIRONMENT
+    Dylinker(LcDylinker),
+    /// LC_DYLD_ENVIRONMENT or LC_UNIXTHREAD
+    Thread(LcThread),
+    /// LC_ROUTINES
+    Routines(LcRoutines),
+    /// LC_ROUTINES_64
+    Routines64(LcRoutines64),
+    /// LC_SYMTAB
+    Symtab(LcSymtab),
+    /// LC_DYSYMTAB
+    Dysimtab(LcDysimtab),
     /// LC_PREBIND_CKSUM
     PrebindChekSum(LcPrebindChekSum),
     /// LC_UUID
@@ -74,7 +159,114 @@ pub enum LcVariant {
     SourceVersion(LcSourceVersion),
     /// LC_NOTE
     Note(LcNote),
+    /// Any unknown command type
+    Unknown(u32),
 }
+/*
+impl TryFrom<u32> for LcVariant {
+    type Error = Error;
+
+    fn try_from(value: u32) -> std::result::Result<Self, Self::Error> {
+        match value {
+            LC_SEGMENT => Ok(Self::),
+            LC_SYMTAB => Ok(Self::TYPE),
+            LC_SYMSEG => Ok(Self::TYPE),
+            LC_THREAD => Ok(Self::TYPE),
+            LC_UNIXTHREAD => Ok(Self::TYPE),
+            LC_LOADFVMLIB => Ok(Self::TYPE),
+            LC_IDFVMLIB => Ok(Self::TYPE),
+            LC_IDENT => Ok(Self::TYPE),
+            LC_FVMFILE => Ok(Self::TYPE),
+            LC_PREPAGE => Ok(Self::TYPE),
+            LC_DYSYMTAB => Ok(Self::TYPE),
+            LC_LOAD_DYLIB => Ok(Self::TYPE),
+            LC_ID_DYLIB => Ok(Self::TYPE),
+            LC_LOAD_DYLINKER => Ok(Self::TYPE),
+            LC_ID_DYLINKER => Ok(Self::TYPE),
+            LC_PREBOUND_DYLIB => Ok(Self::TYPE),
+            LC_ROUTINES => Ok(Self::TYPE),
+            LC_SUB_FRAMEWORK => Ok(Self::TYPE),
+            LC_SUB_UMBRELLA => Ok(Self::TYPE),
+            LC_SUB_CLIENT => Ok(Self::TYPE),
+            LC_SUB_LIBRARY => Ok(Self::TYPE),
+            LC_TWOLEVEL_HINTS => Ok(Self::TYPE),
+            LC_PREBIND_CKSUM => Ok(Self::TYPE),
+            LC_LOAD_WEAK_DYLIB => Ok(Self::TYPE),
+            LC_SEGMENT_64 => Ok(Self::TYPE),
+            LC_ROUTINES_64 => Ok(Self::TYPE),
+            LC_UUID => Ok(Self::TYPE),
+            LC_RPATH => Ok(Self::TYPE),
+            LC_CODE_SIGNATURE => Ok(Self::TYPE),
+            LC_SEGMENT_SPLIT_INFO => Ok(Self::TYPE),
+            LC_REEXPORT_DYLIB => Ok(Self::TYPE),
+            LC_LAZY_LOAD_DYLIB => Ok(Self::TYPE),
+            LC_ENCRYPTION_INFO => Ok(Self::TYPE),
+            LC_DYLD_INFO => Ok(Self::TYPE),
+            LC_DYLD_INFO_ONLY => Ok(Self::TYPE),
+            LC_LOAD_UPWARD_DYLIB => Ok(Self::TYPE),
+            LC_VERSION_MIN_MACOSX => Ok(Self::TYPE),
+            LC_VERSION_MIN_IPHONEOS => Ok(Self::TYPE),
+            LC_FUNCTION_STARTS => Ok(Self::TYPE),
+            LC_DYLD_ENVIRONMENT => Ok(Self::TYPE),
+            LC_MAIN => Ok(Self::TYPE),
+            LC_DATA_IN_CODE => Ok(Self::TYPE),
+            LC_SOURCE_VERSION => Ok(Self::TYPE),
+            LC_DYLIB_CODE_SIGN_DRS => Ok(Self::TYPE),
+            LC_ENCRYPTION_INFO_64 => Ok(Self::TYPE),
+            LC_LINKER_OPTION => Ok(Self::TYPE),
+            LC_LINKER_OPTIMIZATION_HINT => Ok(Self::TYPE),
+            LC_VERSION_MIN_TVOS => Ok(Self::TYPE),
+            LC_VERSION_MIN_WATCHOS => Ok(Self::TYPE),
+            LC_NOTE => Ok(Self::TYPE),
+            LC_BUILD_VERSION => Ok(Self::TYPE),
+            _ => Ok(Self::Unknown(value)),
+        }
+    }
+}
+*/
+
+/// LC_SEGMENT
+pub struct LcSegment;
+
+/// LC_SEGMENT_64
+pub struct LcSegment64;
+
+/// LC_ID_DYLIB, LC_LOAD_{,WEAK_}DYLIB, LC_REEXPORT_DYLIB
+pub struct LcDylib;
+
+/// LC_SUB_FRAMEWORK
+pub struct LcSubframework;
+
+/// LC_SUB_CLIENT
+pub struct LcSubclient;
+
+/// LC_SUB_UMBRELLA
+pub struct LcSubumbrella;
+
+/// LC_SUB_LIBRARY
+pub struct LcSublibrary;
+
+/// LC_PREBOUND_DYLIB
+pub struct LcPreboundDylib;
+
+/// LC_ID_DYLINKER, LC_LOAD_DYLINKER, LC_DYLD_ENVIRONMENT
+pub struct LcDylinker;
+
+/// LC_DYLD_ENVIRONMENT or LC_UNIXTHREAD
+pub struct LcThread;
+
+/// LC_ROUTINES
+pub struct LcRoutines;
+
+/// LC_ROUTINES_64
+pub struct LcRoutines64;
+
+/// LC_SYMTAB
+pub struct LcSymtab;
+
+/// LC_DYSYMTAB
+pub struct LcDysimtab;
+
 
 /// prebind_cksum_command
 pub struct LcPrebindChekSum {
@@ -237,7 +429,7 @@ pub struct LcNote {
     /// offset
     offset: u64,
     /// size
-    size: u64
+    size: u64,
 }
 
 pub enum LinkEditDataVariant {
@@ -256,8 +448,12 @@ pub enum LinkEditDataVariant {
 }
 
 pub enum VersionMinVariant {
-    MacOsX,   // LC_VERSION_MIN_MACOSX
-    IphoneOs, // LC_VERSION_MIN_IPHONEOS
-    WatchOs,  // LC_VERSION_MIN_WATCHOS
-    TvOs,     // LC_VERSION_MIN_TVOS
+    /// LC_VERSION_MIN_MACOSX
+    MacOsX,
+    /// LC_VERSION_MIN_IPHONEOS
+    IphoneOs,
+    /// LC_VERSION_MIN_WATCHOS
+    WatchOs,
+    /// LC_VERSION_MIN_TVOS
+    TvOs,
 }
