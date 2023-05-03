3ffbb71e9650049d90ea8bdba105cd6f98dd098f
Artem Sechko <arsynthdev@gmail.com>
2023-05-03 15:10:04 +0700

    Colored output

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,44 @@
-use schnauzer::*;
-use std::path::Path;
-use std::fmt::*;
 use colored::{self, Colorize};
+use schnauzer::*;
+use std::fmt::*;
+use std::path::Path;
+
+struct DashLine {
+    head: String,
+    body: String,
+    tail: String,
+}
+
+impl DashLine {
+    pub fn new(head: &str, body: &str, tail: &str) -> Self {
+        DashLine {
+            head: head.to_string(),
+            body: body.to_string(),
+            tail: tail.to_string(),
+        }
+    }
+
+    pub fn new_header() -> Self {
+        let body = format!("{}", "----|");
+        DashLine::new("|", &body, "----")
+    }
+
+    pub fn new_field() -> Self {
+        let tail = format! {"{}{}", "|", "*".dimmed()};
+        let body = format!("{}", "    ".dimmed());
+        DashLine::new("|", &body, &tail)
+    }
+}
+
+impl DashLine {
+    pub fn get_string(&self, size: usize) -> String {
+        if size == 0 {
+            return "".to_string();
+        }
+
+        format!("{}{}{}", self.head, self.body.repeat(size - 1), self.tail)
+    }
+}
 
 fn main() {
     let mut args = std::env::args();
@@ -43,20 +80,20 @@ fn handle_object(obj: ObjectType) {
 }
 
 fn handle_fat(fat: FatObject) {
-    header("Fat:", 0);
+    out_header("Fat:", 0);
     for arch in fat.arch_iterator() {
         handle_arch(arch);
     }
 }
 
 fn handle_arch(arch: FatArch) {
-    header("Fat arch:", 1);
+    out_header("Fat arch:", 1);
 
-    field("cputype", &arch.cputype.to_string(), 1);
-    field("cpusubtype", &arch.masked_cpu_subtype().to_string(), 1);
-    field("offset", &arch.offset.to_string(), 1);
-    field("size", &arch.size.to_string(), 1);
-    field("align", &arch.align.to_string(), 1);
+    out_field("cputype", &arch.cputype.to_string(), 1);
+    out_field("cpusubtype", &arch.masked_cpu_subtype().to_string(), 1);
+    out_field("offset", &arch.offset.to_string(), 1);
+    out_field("size", &arch.size.to_string(), 1);
+    out_field("align", &arch.align.to_string(), 1);
 
     handle_macho(arch.object().unwrap(), true);
 }
@@ -66,49 +103,39 @@ fn handle_macho(macho: MachObject, nested: bool) {
         true => 2,
         false => 1,
     };
-    header("Mach header:", level);
+    out_header("Mach header:", level);
 
     let h = macho.header();
-    field("magic", &h.magic.raw_value().hex_string(9), level);
-    field("cputype", &h.masked_cpu_subtype().to_string(), level);
-    field("filetype", &h.file_type().to_string(), level);
-    field("ncmds", &h.ncmds.to_string(), level);
-    field("flags", &h.flags.hex_string(9), level);
+    out_field("magic", &h.magic.raw_value().hex_string(9), level);
+    out_field("cputype", &h.masked_cpu_subtype().to_string(), level);
+    out_field("filetype", &h.file_type().to_string(), level);
+    out_field("ncmds", &h.ncmds.to_string(), level);
+    out_field("flags", &h.flags.hex_string(9), level);
 
-    header("Load commands:", level);
+    out_header("Load commands:", level + 1);
     for cmd in macho.load_commands_iterator() {
         handle_load_command(cmd, level + 1);
     }
 }
 
 fn handle_load_command(cmd: LoadCommand, level: usize) {
-    field("cmd", &fmt_ext::load_commang_to_string(cmd.cmd), level);
-    field("cmdsize", &cmd.cmdsize.to_string(), level);
+    out_field("cmd", &fmt_ext::load_commang_to_string(cmd.cmd), level);
+    out_field("cmdsize", &cmd.cmdsize.to_string(), level);
 }
 
-fn header(hdr: &str, level: usize) {
-    if level > 0 {
-        print!("|");
-    }
-    if level > 1 {
-        print!("{}", "    |".repeat(level - 1));
-    }
-    if level > 0 {
-        print!("----");
-    }
+fn out_header(hdr: &str, level: usize) {
+    print!("{}", DashLine::new_header().get_string(level));
     print!("{hdr}");
     println!("");
 }
 
-fn field(name: &str, value: &str, level: usize) {
-    field_delimited(name, value, level, "\n");
+fn out_field(name: &str, value: &str, level: usize) {
+    out_field_delimited(name, value, level, "\n");
 }
 
-fn field_delimited(name: &str, value: &str, level: usize, delimiter: &str) {
-    print!("|");
-    if level > 0 {
-        print!("{}", "    |".repeat(level));
-    }
+fn out_field_delimited(name: &str, value: &str, level: usize, delimiter: &str) {
+    print!("{}", DashLine::new_field().get_string(level + 1));
+
     if name.len() > 0 || value.len() > 0 {
         print!("{name}: {}", value.yellow());
     }
